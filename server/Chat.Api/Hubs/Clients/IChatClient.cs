650ae711cc60e465e3aeb7fea806518a264ad216
Ali <ali99aqel@gmail.com>
2023-08-14 23:11:11 +0300

    added MessageHub

---
+++ server/Chat.Api/Hubs/Clients/IChatClient.cs
@@ -0,0 +1,11 @@
+﻿
+using Chat.Domain;
+using Chat.Domain.Entities;
+
+namespace Chat.Api.Hubs.Clients
+{
+    public interface IChatClient
+    {
+        Task ReceiveMessage(Message message);
+    }
+}
