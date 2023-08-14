9eff364572424758ea9efc71ea0c60e9c5094469
Ali <ali99aqel@gmail.com>
2023-08-14 23:13:37 +0300

    added MessageDataContext and MongoDbContext

---
+++ server/Chat.Infrastructure/Configurations/ConfigureMongo.cs
@@ -0,0 +1,16 @@
+﻿using Chat.Interfaces;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Chat.Infrastructure.Configurations;
+
+public static class ConfigureMongo
+{
+    public static IServiceCollection AddMongo(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.Configure<MongoDbSettings>(configuration.GetSection("MongoDb"));
+        services.AddSingleton<IMongoDbContext, MongoDbContext>();
+
+        return services;
+    }
+}
---
+++ server/Chat.Infrastructure/MessageDataContext.cs
@@ -0,0 +1,19 @@
+﻿using Chat.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Chat.Infrastructure;
+
+public class MessageDataContext : DbContext
+{
+    public MessageDataContext() {}
+    public MessageDataContext(DbContextOptions options) : base(options) {}
+
+    public DbSet<Domain.Entities.Chat> Chats { get; set; }
+    public DbSet<Message> Messages { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.UseSerialColumns();
+        base.OnModelCreating(modelBuilder);
+    }
+}
